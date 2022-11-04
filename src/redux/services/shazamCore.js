e64fc8577e3867c6de188728354626915cb28674
mscalel <msksaul@gmail.com>
2022-11-04 17:47:56 -0600

    Data Fetching

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', '436cfed03bmsh610896c110ddc21p1b94a9jsnc900e5e7f3be')
+      return headers
+    }
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  })
+})
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi
